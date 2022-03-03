2cfd3ca3bc79d2612669342f7510bdc5c51f6236
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-03 23:29:37 +0300

    feat(modal): focus trap

--- src/widget/modal/index.tsx
+++ src/widget/modal/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import FocusTrap from 'focus-trap-react';
 import styles from './styles.module.scss';
 
 export const Modal: React.FC = ({ children }) => (
-  // eslint-disable-next-line jsx-a11y/no-static-element-interactions,jsx-a11y/no-noninteractive-element-interactions
-  <div
-    className={styles.Modal__Background}
-    aria-modal="true"
-    role="dialog"
-    onKeyDown={(event) => console.log(event.key === 'Escape')}
-  >
-    <div className={styles.Modal__Card}>{children}</div>
-  </div>
+  <FocusTrap>
+    {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+    <div
+      className={styles.Modal__Background}
+      aria-modal="true"
+      role="dialog"
+      onKeyDown={(event) => console.log(event.key === 'Escape')}
+    >
+      <div className={styles.Modal__Card}>{children}</div>
+    </div>
+  </FocusTrap>
 );
