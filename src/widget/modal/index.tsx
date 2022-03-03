1b9d3814f118548bd9a7ad988db7531c5231c054
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-04 00:03:14 +0300

    feat(modal): helmet title

--- src/widget/modal/index.tsx
+++ src/widget/modal/index.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import FocusTrap from 'focus-trap-react';
+import { Helmet } from 'react-helmet';
 import styles from './styles.module.scss';
 
-export const Modal: React.FC = ({ children }) => (
-  <FocusTrap>
-    {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-    <div
-      className={styles.Modal__Background}
-      aria-modal="true"
-      role="dialog"
-      onKeyDown={(event) => console.log(event.key === 'Escape')}
-    >
-      <div className={styles.Modal__Card}>{children}</div>
-    </div>
-  </FocusTrap>
+type ModalProps = {
+  title?: string;
+};
+
+export const Modal: React.FC<ModalProps> = ({ children, title }) => (
+  <>
+    {title && (
+      <Helmet>
+        <title>title</title>
+      </Helmet>
+    )}
+    <FocusTrap>
+      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+      <div
+        className={styles.Modal__Background}
+        aria-modal="true"
+        role="dialog"
+        onKeyDown={(event) => console.log(event.key === 'Escape')}
+      >
+        <div className={styles.Modal__Card}>{children}</div>
+      </div>
+    </FocusTrap>
+  </>
 );
+
+Modal.defaultProps = {
+  title: '',
+};
