053da6501dc93c19c4103e6e5e95d5f4847fa19c
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-04 00:00:21 +0300

    chore(styles): remove redundant styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter } from 'react-router-dom';
+import { Menu } from './widget/menu';
+import { Search } from './widget/search';
+import { Modal } from './widget/modal';
+import { Auth } from './widget/auth';
+
+const AppLayout = () => (
+  <header>
+    Header
+    <Menu /> <Search />
+    <Modal>
+      <Auth />
+    </Modal>
+  </header>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <AppLayout />
+      </BrowserRouter>
     </div>
   );
 }
