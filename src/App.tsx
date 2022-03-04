8aba9b9863514f8dc89b5207f697a5590aed0f12
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-04 19:24:49 +0300

    chore(app): improvements

--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,24 @@ import { Search } from './widget/search';
 import { Modal } from './widget/modal';
 import { Auth } from './widget/auth';
 
-const AppLayout = () => (
-  <header>
-    Header
-    <Menu /> <Search />
-    <Modal>
-      <Auth />
-    </Modal>
-  </header>
-);
+const AppLayout = () => {
+  const [modal, showModal] = React.useState(false);
+
+  return (
+    <header>
+      Header
+      <Menu /> <Search />
+      <button type="button" onClick={() => showModal(true)}>
+        Enter
+      </button>
+      {modal && (
+        <Modal title="Авторизация - Вход" onClose={() => showModal(false)}>
+          <Auth />
+        </Modal>
+      )}
+    </header>
+  );
+};
 
 function App() {
   return (
--- src/widget/modal/index.tsx
+++ src/widget/modal/index.tsx
@@ -1,32 +1,62 @@
 import React from 'react';
 import FocusTrap from 'focus-trap-react';
 import { Helmet } from 'react-helmet';
+import { createPortal } from 'react-dom';
 import styles from './styles.module.scss';
+import { SquareButton } from '../../shared/square-button';
 
 type ModalProps = {
   title?: string;
+  onClose: () => void;
 };
 
-export const Modal: React.FC<ModalProps> = ({ children, title }) => (
-  <>
-    {title && (
-      <Helmet>
-        <title>title</title>
-      </Helmet>
-    )}
-    <FocusTrap>
-      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
-      <div
-        className={styles.Modal__Background}
-        aria-modal="true"
-        role="dialog"
-        onKeyDown={(event) => console.log(event.key === 'Escape')}
-      >
-        <div className={styles.Modal__Card}>{children}</div>
-      </div>
-    </FocusTrap>
-  </>
-);
+export const Modal: React.FC<ModalProps> = ({ children, title, onClose }) => {
+  const ModalElement = (
+    <>
+      {title && (
+        <Helmet>
+          <title>{title}</title>
+        </Helmet>
+      )}
+      <FocusTrap>
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+        <div
+          className={styles.Modal__Background}
+          aria-modal="true"
+          role="dialog"
+          onKeyDown={(event) => event.key === 'Escape' && onClose()}
+        >
+          <div className={styles.Modal__Card}>
+            <div>
+              <label
+                htmlFor="modalCloseButton"
+                className={styles.visually_hidden}
+              >
+                Закрыть
+              </label>
+              <SquareButton
+                type="button"
+                onClick={onClose}
+                id="modalCloseButton"
+              >
+                X
+              </SquareButton>
+            </div>
+            {children}
+          </div>
+        </div>
+      </FocusTrap>
+    </>
+  );
+
+  const root = document.getElementById('modal-root');
+  if (!root) {
+    console.log('modal-root element not found');
+    return null;
+  }
+
+  return createPortal(ModalElement, root);
+};
 
 Modal.defaultProps = {
   title: '',
