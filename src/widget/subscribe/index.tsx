3d0b4e9f8432d6796a0daab7b66b742fd51db533
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-06 21:37:17 +0300

    feat(a11y): subscribe form

---
+++ src/widget/subscribe/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './styles.module.scss';
+import { Button } from '../../shared/button';
+import { UnderlinedInput } from '../../shared/underlined-input';
+
+export const Subscribe: React.FC = () => (
+  <form className={styles.Subscribe}>
+    <div className={styles.Subscribe__Input}>
+      <UnderlinedInput type="email" placeholder="Email" width={303} />
+      <Button type="submit" size="small">
+        Подписаться
+      </Button>
+    </div>
+
+    <div>
+      <input type="checkbox" id="subscribe-alignment" />
+      <label htmlFor="subscribe-alignment">
+        Согласен на обработку персональных данных
+      </label>
+    </div>
+  </form>
+);
