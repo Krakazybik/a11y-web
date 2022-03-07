18fc18e6c0862ce2b5148edd7bea9b53cbab8951
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-07 21:10:15 +0300

    feat(a11y): language switch html lange change

--- src/widget/language-switch/index.tsx
+++ src/widget/language-switch/index.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
+import { Helmet } from 'react-helmet';
 import { RovingRadioGroup } from '../../shared/roving-radio-group';
 
 export const LanguageSwitch: React.FC = () => {
+  const [lang, setLang] = React.useState('ru');
+
   const radios = [
-    { id: 'language-switch-en', label: 'EN', ariaText: 'English' },
     { id: 'language-switch-ru', label: 'RU', ariaText: 'Русский' },
+    { id: 'language-switch-en', label: 'EN', ariaText: 'English' },
   ];
 
+  const handleSwitch = (value: number) => {
+    setLang(!value ? 'ru' : 'en');
+  };
+
   return (
-    <RovingRadioGroup
-      id="language-switch"
-      ariaGroupCaption="Выбор языка сайта"
-      radioItems={radios}
-      onSwitch={(value) => console.log(`Selected: ${value}`)}
-    />
+    <>
+      <Helmet htmlAttributes={{ lang }} />
+      <RovingRadioGroup
+        id="language-switch"
+        ariaGroupCaption="Выбор языка сайта"
+        radioItems={radios}
+        onSwitch={handleSwitch}
+      />
+    </>
   );
 };
