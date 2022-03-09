e97a767d4e7a28a397d8823a83aab5f2b7b272c9
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-10 00:43:31 +0300

    fix(a11y): hide search labels

--- src/widget/search/index.tsx
+++ src/widget/search/index.tsx
@@ -1,24 +1,30 @@
 import React from 'react';
 import searchLogo from './assets/search.svg';
 import styles from './styles.module.scss';
+import { AriaSpan } from '../../shared/ui/aria-span';
 
 // TODO: ROLE SEARCH LANDMARK
+// TODO: fix labels
 export const Search = () => (
   <form
     className={styles.Search_Wrapper}
     role="search"
     aria-labelledby="search-label"
   >
-    <label
-      id="search-label"
-      htmlFor="input-search"
-      className={styles.visually_hidden}
+    <AriaSpan id="search-label" text="Поиск" />
+    <AriaSpan id="search-action" text="Введите значение для поиска" />
+    <input
+      className={styles.Search}
+      type="search"
+      aria-labelledby="search-action"
+    />
+    <button
+      className={styles.Search_Button}
+      type="submit"
+      aria-labelledby="search-result"
     >
-      Поиск
-    </label>
-    <input className={styles.Search} type="search" id="input-search" />
-    <button className={styles.Search_Button} type="submit">
       <img src={searchLogo} alt="" aria-hidden />
+      <AriaSpan id="search-result" text="Показать результат поиска" />
     </button>
   </form>
 );
