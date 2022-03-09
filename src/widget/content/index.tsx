35db9e1e3a14f01d5a84fcac616c541b1b2cbf82
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-09 22:55:56 +0300

    feat(a11y): content anchor id

--- src/widget/content/index.tsx
+++ src/widget/content/index.tsx
@@ -2,5 +2,7 @@ import React from 'react';
 import styles from './styles.module.scss';
 
 export const Content: React.FC = ({ children }) => (
-  <div className={styles.Content}>{children}</div>
+  <main id="main-content" className={styles.Content} tabIndex={-1}>
+    {children}
+  </main>
 );
