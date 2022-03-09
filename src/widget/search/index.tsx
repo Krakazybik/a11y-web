f61a8e3b4050f2c3596c37569760c7bb80f81a24
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-09 22:52:19 +0300

    fix(a11y): search button

--- src/widget/search/index.tsx
+++ src/widget/search/index.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import searchLogo from './assets/search.svg';
 import styles from './styles.module.scss';
 
+// TODO: ROLE SEARCH LANDMARK
 export const Search = () => (
-  <>
-    <label htmlFor="input-search" className={styles.visually_hidden}>
+  <form
+    className={styles.Search_Wrapper}
+    role="search"
+    aria-labelledby="search-label"
+  >
+    <label
+      id="search-label"
+      htmlFor="input-search"
+      className={styles.visually_hidden}
+    >
       Поиск
     </label>
     <input className={styles.Search} type="search" id="input-search" />
-  </>
+    <button className={styles.Search_Button} type="submit">
+      <img src={searchLogo} alt="" aria-hidden />
+    </button>
+  </form>
 );
