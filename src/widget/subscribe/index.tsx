921bdd215a1f84cca21e898c947e923328ec0974
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-09 22:53:14 +0300

    fix(a11y): subscribe semantic

--- src/widget/subscribe/index.tsx
+++ src/widget/subscribe/index.tsx
@@ -4,19 +4,22 @@ import { Button } from '../../shared/ui/button';
 import { UnderlinedInput } from '../../shared/ui/underlined-input';
 
 export const Subscribe: React.FC = () => (
-  <form className={styles.Subscribe}>
-    <div className={styles.Subscribe__Input}>
-      <UnderlinedInput type="email" placeholder="Email" width={303} />
-      <Button type="submit" size="small">
-        Подписаться
-      </Button>
-    </div>
+  <aside aria-labelledby="subscribe-heading">
+    <h2 id="subscribe-heading">Подписка на новости</h2>
+    <form className={styles.Subscribe}>
+      <div className={styles.Subscribe__Input}>
+        <UnderlinedInput type="email" placeholder="Email" width={303} />
+        <Button type="submit" size="small">
+          Подписаться
+        </Button>
+      </div>
 
-    <div>
-      <input type="checkbox" id="subscribe-alignment" />
-      <label htmlFor="subscribe-alignment">
-        Согласен на обработку персональных данных
-      </label>
-    </div>
-  </form>
+      <div>
+        <input type="checkbox" id="subscribe-alignment" />
+        <label htmlFor="subscribe-alignment">
+          Согласен на обработку персональных данных
+        </label>
+      </div>
+    </form>
+  </aside>
 );
