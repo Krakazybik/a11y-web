6816a0748d03dd8ef242d202b3a5714c6ee6a1fb
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-11 02:40:25 +0300

    fix(a11y): subscribe input order

--- src/widget/footer/subscribe/index.tsx
+++ src/widget/footer/subscribe/index.tsx
@@ -1,11 +1,14 @@
-import React, { FormEvent, useEffect, useState } from 'react';
+import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import { Alert } from 'shared/ui/alert';
 import { Button } from 'shared/ui/button';
 import { UnderlinedInput } from 'shared/ui/underlined-input';
 import styles from './styles.module.scss';
 
+// TODO: Agree condition alert
+
 export const Subscribe: React.FC = () => {
   const [alert, showAlert] = useState(false);
+  const [alignment, setAlignment] = useState(false);
 
   useEffect(() => {
     const timer = setTimeout(() => showAlert(false), 8000);
@@ -15,7 +18,13 @@ export const Subscribe: React.FC = () => {
 
   const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    showAlert(true);
+    if (alignment) {
+      showAlert(true);
+    }
+  };
+
+  const handleChangeAlignment = (event: ChangeEvent<HTMLInputElement>) => {
+    setAlignment(event.target.checked);
   };
 
   return (
@@ -23,17 +32,20 @@ export const Subscribe: React.FC = () => {
       <h2 id="subscribe-heading">Подписка на новости</h2>
       <form className={styles.Subscribe} onSubmit={handleSubmit}>
         <div className={styles.Subscribe__Input}>
-          <UnderlinedInput type="email" placeholder="Email" width={303} />
+          <UnderlinedInput type="email" placeholder="Email" />
+          <div>
+            <input
+              type="checkbox"
+              id="subscribe-alignment"
+              onChange={handleChangeAlignment}
+            />
+            <label htmlFor="subscribe-alignment">
+              Согласен на обработку персональных данных
+            </label>
+          </div>
           <Button type="submit" size="small">
             Подписаться
           </Button>
-        </div>
-
-        <div>
-          <input type="checkbox" id="subscribe-alignment" />
-          <label htmlFor="subscribe-alignment">
-            Согласен на обработку персональных данных
-          </label>
         </div>
       </form>
       {alert && <Alert text="Вы успешно подписались на новостную рассылку." />}
