8cc19b9c8d91e28458281f9b76153ab7e8341a50
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-10 22:56:49 +0300

    fix(a11y): history polite

--- src/widget/history/index.tsx
+++ src/widget/history/index.tsx
@@ -14,9 +14,14 @@ export const History: React.FC<HistoryProps> = ({
   partIndex = 0,
 }) => {
   const [showMore, setShowMore] = React.useState(false);
+  const showMoreRef = React.useRef<HTMLParagraphElement>(null);
+
+  React.useEffect(() => {
+    showMoreRef.current?.focus();
+  }, [setShowMore]);
 
   return (
-    <article className={styles.History}>
+    <article className={styles.History} aria-live="polite" aria-atomic>
       <h3 className={styles.visually_hidden}>{header}</h3>
       <p>{partIndex ? text.slice(0, partIndex) : text}</p>
       {(partIndex && !showMore && (
@@ -29,9 +34,13 @@ export const History: React.FC<HistoryProps> = ({
         </button>
       )) ||
         ''}
-      {partIndex && showMore && (
-        <p aria-live="polite">{text.slice(partIndex)}</p>
-      )}
+      <div aria-live="polite" aria-atomic>
+        {partIndex && showMore && (
+          <p aria-atomic ref={showMoreRef}>
+            {text.slice(partIndex)}
+          </p>
+        )}
+      </div>
     </article>
   );
 };
