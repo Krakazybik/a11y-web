7b11381c840c4d22fc39563f49810ba0f10856c9
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-12 22:37:37 +0300

    fix(a11y): search anchor and focus result

--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,34 +1,39 @@
 import React from 'react';
 import { PageHead } from 'shared/ui/page-head';
+import { useAnchor } from 'shared/lib';
 import styles from './styles.module.scss';
 
-export const Search = () => (
-  <>
-    <PageHead header="Государственный музей имени Пушкина" />
-    <section>
-      <h2>Найденные билеты:</h2>
-      <table className={styles.Result_Table}>
-        <caption>Найденные выставки</caption>
-        <thead>
-          <tr>
-            <th>Выставка</th>
-            <th>Дата</th>
-            <th>Цена, рублей</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>От Дюрера до Матисса.</td>
-            <td>27 сентября</td>
-            <td>900</td>
-          </tr>
-          <tr>
-            <td>Мумии Древнего Египта. Искусство бессмертия</td>
-            <td>28 сентября</td>
-            <td>1200</td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
-  </>
-);
+export const Search = () => {
+  useAnchor();
+
+  return (
+    <>
+      <PageHead header="Результаты поиска" />
+      <section>
+        <h1 id="search-result">Найденные билеты:</h1>
+        <table className={styles.Result_Table}>
+          <caption>Найденные выставки</caption>
+          <thead>
+            <tr>
+              <th>Выставка</th>
+              <th>Дата</th>
+              <th>Цена, рублей</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>От Дюрера до Матисса.</td>
+              <td>27 сентября</td>
+              <td>900</td>
+            </tr>
+            <tr>
+              <td>Мумии Древнего Египта. Искусство бессмертия</td>
+              <td>28 сентября</td>
+              <td>1200</td>
+            </tr>
+          </tbody>
+        </table>
+      </section>
+    </>
+  );
+};
