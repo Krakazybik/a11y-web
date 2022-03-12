363dc355a4c24fe7a3acd4e4dc7d16c183b91c0a
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-12 22:41:41 +0300

    feat(a11y): navigate to result

--- src/widget/header/search/index.tsx
+++ src/widget/header/search/index.tsx
@@ -1,30 +1,41 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { AriaSpan } from 'shared/ui/aria-span';
 import searchLogo from './assets/search.svg';
 import styles from './styles.module.scss';
 
 // TODO: ROLE SEARCH LANDMARK
 // TODO: fix labels
-export const Search = () => (
-  <form
-    className={styles.Search_Wrapper}
-    role="search"
-    aria-labelledby="search-label"
-  >
-    <AriaSpan id="search-label" text="Поиск" />
-    <AriaSpan id="search-action" text="Введите значение для поиска" />
-    <input
-      className={styles.Search}
-      type="search"
-      aria-labelledby="search-action"
-    />
-    <button
-      className={styles.Search_Button}
-      type="submit"
-      aria-labelledby="search-result"
+export const Search = () => {
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    navigate('/search/#main-content');
+  };
+
+  return (
+    <form
+      className={styles.Search_Wrapper}
+      role="search"
+      aria-labelledby="search-label"
+      onSubmit={handleSubmit}
     >
-      <img src={searchLogo} alt="" aria-hidden />
-      <AriaSpan id="search-result" text="Показать результат поиска" />
-    </button>
-  </form>
-);
+      <AriaSpan id="search-label" text="Найти билеты на выставки" />
+      <AriaSpan id="search-action" text="Введите значение для поиска" />
+      <input
+        className={styles.Search}
+        type="search"
+        aria-labelledby="search-action"
+      />
+      <button
+        className={styles.Search_Button}
+        type="submit"
+        aria-labelledby="search-result"
+      >
+        <img src={searchLogo} alt="" aria-hidden />
+        <AriaSpan id="search-result" text="Показать результат поиска" />
+      </button>
+    </form>
+  );
+};
