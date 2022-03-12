36196770797949f06a2a6ad98c14d022a879cdac
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-12 22:38:33 +0300

    feat(a11y): new pages navigation routes

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { AntiCorruption } from 'pages/anti-corruption';
+import { Disabilities } from 'pages/disabilities';
 import { Main } from 'pages/main';
 import { Search } from 'pages/search';
+import { Tickets } from 'pages/tickets';
 import { Header } from 'widget/header';
 import { Content } from 'shared/ui/content';
 import { Footer } from 'widget/footer';
 import './styles.scss';
+import { Visitors } from '../pages/visitors';
 
 const AppLayout: React.FC = ({ children }) => (
   <>
@@ -23,6 +27,10 @@ function App() {
           <Routes>
             <Route path="/" element={<Main />} />
             <Route path="/search" element={<Search />} />
+            <Route path="/anti-corruption" element={<AntiCorruption />} />
+            <Route path="/tickets" element={<Tickets />} />
+            <Route path="/disabilities" element={<Disabilities />} />
+            <Route path="/visitors" element={<Visitors />} />
           </Routes>
         </AppLayout>
       </BrowserRouter>
