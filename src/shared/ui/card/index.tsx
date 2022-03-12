63f769b3942d9bcfd2d9322dff9cded9c2d33cca
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-12 22:38:49 +0300

    fix(a11y): remove card tabindex

--- src/shared/ui/card/index.tsx
+++ src/shared/ui/card/index.tsx
@@ -3,7 +3,5 @@ import styles from './styles.module.scss';
 
 export const Card: React.FC = ({ children }) => (
   // eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex
-  <article tabIndex={0} className={styles.Card}>
-    {children}
-  </article>
+  <div className={styles.Card}>{children}</div>
 );
