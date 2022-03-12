8585443f1e20e37d4bbc2f341492dfc6a256b192
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-12 22:40:46 +0300

    feat(a11y): links to articles

--- src/widget/footer/links/index.tsx
+++ src/widget/footer/links/index.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import styles from './styles.module.scss';
 
 // TODO: No landmark in tree
+// TODO: Underline style
 export const Links: React.FC = () => (
   <nav className={styles.Links__Nav} aria-labelledby="heading-links">
     <h2 id="heading-links" className={styles.visually_hidden}>
@@ -10,13 +11,17 @@ export const Links: React.FC = () => (
     </h2>
     <ul>
       <li>
-        <Link to="/">Посетителям с ограниченными возможностями</Link>
+        <Link to="/disabilities/#main-content">
+          Посетителям с ограниченными возможностями
+        </Link>
       </li>
       <li>
-        <Link to="/">Билеты и льготы</Link>
+        <Link to="/tickets/#main-content">Билеты и льготы</Link>
       </li>
       <li>
-        <Link to="/">Противодействие коррупции</Link>
+        <Link to="/anti-corruption/#main-content">
+          Противодействие коррупции
+        </Link>
       </li>
     </ul>
   </nav>
