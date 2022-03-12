55a3be2f40bd620531a53d70329f8546fb3853de
Evgeniy Geraskin <daem0nbfg@gmail.com>
2022-03-12 22:43:04 +0300

    feat(a11y): useAnchor hook

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,19 @@
+import React from 'react';
 import lodash from 'lodash';
+import { useLocation } from 'react-router-dom';
 
 export const getUniqueId = (prefix: string) => `${prefix}-${lodash.uniqueId()}`;
+
+export const useAnchor = () => {
+  const { hash } = useLocation();
+
+  React.useEffect(() => {
+    const anchorElement = hash && document.getElementById(hash.slice(1));
+    if (anchorElement) {
+      anchorElement.scrollIntoView({ behavior: 'smooth' });
+      anchorElement.focus();
+    } else {
+      console.log('Anchor element not found');
+    }
+  }, [hash]);
+};
